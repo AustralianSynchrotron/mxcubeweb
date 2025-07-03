c73652125179492c548948457599a7913f555a88
Noah Kim <dongwoo_333@hanmail.net>
2025-07-03 15:42:30 +1000

    fix: update_shapes in redux

--- ui/src/reducers/shapes.js
+++ ui/src/reducers/shapes.js
@@ -34,16 +34,17 @@ function shapesReducer(state = INITIAL_STATE, action = {}) {
         // when grid shapes are updated, do not update screenCoord
         // as the screenCoord gets weirdly updated by other motors
         // (e.g., Omega, Y, and so on), but not the X motor.
-        const updatedShapes = { shape };
-        Object.keys(updatedShapes).forEach((key) => {
+        const updatedShape = { ...shape };
+        Object.keys(updatedShape).forEach((key) => {
           // assume we can use label = 'Grid' or t = 'G' according to the payload
-          if (updatedShapes[key].label === 'Grid') {
-            const previousScreenCoord = state.shapes[key].screenCoord;
-            updatedShapes[key].screenCoord = previousScreenCoord;
+          if (key === 'label' && updatedShape[key] === 'Grid') {
+            const previousScreenCoord =
+              state.shapes[updatedShape.id].screenCoord;
+            updatedShape.screenCoord = previousScreenCoord;
           }
         });
 
-        shapes[updatedShapes.id] = updatedShapes;
+        shapes[updatedShape.id] = updatedShape;
       });
 
       return { ...state, shapes };
