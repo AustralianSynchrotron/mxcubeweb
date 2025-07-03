6945cba664df312a4c1aed413298a93a61660faf
Noah Kim <dongwoo_333@hanmail.net>
2025-07-03 15:03:00 +1000

    chore(shapes): do not update screen coord when it gets UPDATE_SHAPES action

--- ui/src/reducers/shapes.js
+++ ui/src/reducers/shapes.js
@@ -31,7 +31,19 @@ function shapesReducer(state = INITIAL_STATE, action = {}) {
       const shapes = { ...state.shapes };
 
       action.shapes.forEach((shape) => {
-        shapes[shape.id] = shape;
+        // when grid shapes are updated, do not update screenCoord
+        // as the screenCoord gets weirdly updated by other motors
+        // (e.g., Omega, Y, and so on), but not the X motor.
+        const updatedShapes = { shape };
+        Object.keys(updatedShapes).forEach((key) => {
+          // assume we can use label = 'Grid' or t = 'G' according to the payload
+          if (updatedShapes[key].label === 'Grid') {
+            const previousScreenCoord = state.shapes[key].screenCoord;
+            updatedShapes[key].screenCoord = previousScreenCoord;
+          }
+        });
+
+        shapes[updatedShapes.id] = updatedShapes;
       });
 
       return { ...state, shapes };
