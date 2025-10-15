777a5d97d454307ab1367a82a07a74b480a9faa7
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2025-10-15 11:09:14 +1100

    remove unused endpoints

--- ui/src/api/labs.js
+++
@@ -1,6 +0,0 @@
-import api from '.';
-
-// Fetch list of labs from LIMS scope. Expected response: array of { id, name } or strings.
-export function fetchLabs() {
-  return api.get('/lims/labs').safeJson();
-}
--- ui/src/api/projects.js
+++
@@ -1,6 +0,0 @@
-import api from '.';
-
-// Fetch list of projects from LIMS scope. Expected response: array of { id, name } or strings.
-export function fetchProjects() {
-  return api.get('/lims/projects').safeJson();
-}
